dbe62923232c1d3674b3ed95a0ed712200678e57
luthfiazzz <luthfiazizi11@gmail.com>
2021-05-19 10:38:36 +0700

    creating url method http

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,27 @@ $router->get('/', function () use ($router) {
 $router->get('/key', function () {
     return Str_random(32);
 });
+
+$router->get('/test', function(){
+    return 'Halo Dunia, get Method!';
+});
+
+$router->post('/posting', function(){
+    return 'Halo, Post Method!';
+});
+
+$router->put('/put', function(){
+    return 'Halo, put Method!';
+});
+
+$router->patch('/patch', function(){
+    return 'Halo, patch Method!';
+});
+
+$router->delete('/delete', function(){
+    return 'Halo, delete Method!';
+});
+
+$router->options('/options', function(){
+    return 'Halo, options Method!';
+});
